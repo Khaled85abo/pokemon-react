df58e92a123ba3675b102e804d7dadfddd9adc5a
julia berglund <jullano9@hotmail.com>
2022-09-13 16:10:24 +0200

    Pokemon names
    All the pokemon names appers on the screen

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {Container} from 'react-bootstrap';
+
+
+//Pages
+import Homepage from './pages/Homepage';
+
+const App = () => {
+  return (
+    <Router>
+    <Container>
+    
+    <Route exact path='/' component={Homepage}/>
+
+
+    </Container>
+    </Router>
+
+  );
+}
+
+export default App;
